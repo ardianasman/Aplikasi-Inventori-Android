a3e64cfe6330796b61a1712133e9509f42c62b78
Leon Susanto <leonsusanto38@gmail.com>
2022-05-24 16:37:41 +0700

    add text

--- project_ambw/lib/main.dart
+++ project_ambw/lib/main.dart
@@ -26,7 +26,7 @@ class _MyAppState extends State<MyApp> {
           margin: EdgeInsets.all(8),
           child: Column(
             children: [
-              Text("Inventory App"),
+              Text("Inventory App for Project AMBW"),
             ],
           ),
         ),
