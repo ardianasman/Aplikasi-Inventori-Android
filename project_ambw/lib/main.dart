785e76e34773e88008a2ea74df7431c85c14613d
Leon Susanto <leonsusanto38@gmail.com>
2022-05-24 16:21:36 +0700

    coba fab

--- project_ambw/lib/main.dart
+++ project_ambw/lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -15,20 +15,23 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Inventory App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Inventory App"),
-        ),
-        body: Container(
-          margin: EdgeInsets.all(8),
-          child: Column(
-            children: [
-              
-            ],
+        title: "Inventory App",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Inventory App"),
           ),
-        ),
-      )
-    );
+          body: Container(
+            margin: EdgeInsets.all(8),
+            child: Column(
+              children: [
+                
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.add),
+          ),
+        ));
   }
 }
