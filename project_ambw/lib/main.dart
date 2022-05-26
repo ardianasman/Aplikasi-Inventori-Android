bd013c60aaa6141201129bac633aa6e4112192f2
ardianasman <ardian1244@gmail.com>
2022-05-27 03:26:38 +0700

    Splash Screen + Pallete

--- project_ambw/lib/main.dart
+++ project_ambw/lib/main.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +17,34 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App Inventory',
+      home: AnimatedSplashScreen(
+        duration: 5000,
+        splash: Icon(
+          Icons.inventory_sharp,
+          color: Colors.white,
+          size: 150,
+        ),
+        nextScreen: MainScreen(),
+        splashTransition: SplashTransition.fadeTransition,
+        pageTransitionType: PageTransitionType.fade,
+        backgroundColor: Color(0xFF264653),
+      ),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
