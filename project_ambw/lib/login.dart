661936f10daea3d05a1e5ce88ebff82755c94bd2
ardianasman <ardian1244@gmail.com>
2022-06-08 20:53:56 +0700

    sign in

--- project_ambw/lib/login.dart
+++ project_ambw/lib/login.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:provider/provider.dart';
+import 'package:project_ambw/provider/google_sign.dart';
 
 class Login extends StatefulWidget {
   const Login({ Key? key }) : super(key: key);
@@ -9,16 +13,28 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: "Login",
-      home: Scaffold(
-        body: Container(
-          padding: EdgeInsets.all(24),
-          child:
-          Text("asd"),
+        title: "Login",
+        home: Scaffold(
+          body: Container(
+            child: Column(
+              children: [
+                FloatingActionButton(
+                  onPressed: () async {
+                    // final provider = Provider.of<GoogleSignInProvider>(context, listen: false);
+                    // provider.googleLogin();
+                    // await _googleSignIn.signIn();
+                    // setState(() {
+                      
+                    // });
+                  },
+                  child: Icon(Icons.mail),
+                ),
+              ],
+            ),
+          )
         ),
-      ),
-    );
+      );
   }
 }
