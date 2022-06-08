7c114b9cd8dcd084dd5a00746a3c7009c6765b7a
Leon Susanto <leonsusanto38@gmail.com>
2022-06-08 20:20:29 +0700

    menambah file

---
+++ project_ambw/lib/detailstok.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailStok extends StatefulWidget {
+  const DetailStok({Key? key}) : super(key: key);
+
+  @override
+  State<DetailStok> createState() => _DetailStokState();
+}
+
+class _DetailStokState extends State<DetailStok> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
